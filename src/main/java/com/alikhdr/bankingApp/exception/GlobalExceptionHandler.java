b23862acf7d0110cc5c65355872cccde881f5fec
alipavel <alipavell@gmail.com>
2026-01-15 00:17:45 +0200

    updated GlobalExceptionHandler

--- src/main/java/com/alikhdr/bankingApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/alikhdr/bankingApp/exception/GlobalExceptionHandler.java
@@ -19,59 +19,66 @@ public class GlobalExceptionHandler
 {
     // Email already exists
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<GlobalResponse> handleEmailExists(EmailAlreadyExistsException ex)
+    public ResponseEntity<GlobalResponse<?>> handleEmailExists(EmailAlreadyExistsException ex)
     {
-        GlobalResponse response = GlobalResponse.builder()
-                .responseCode(HttpStatus.CONFLICT.toString())
-                .responseMessage(ex.getMessage())
-                .build();
-
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(
+                GlobalResponse.builder()
+                        .responseCode("409")
+                        .responseMessage(ex.getMessage())
+                        .build(),
+                HttpStatus.CONFLICT
+        );
     }
 
     // Phone number already exists
     @ExceptionHandler(PhoneNumberAlreadyExistsException.class)
-    public ResponseEntity<GlobalResponse> handlePhoneNumberExists(PhoneNumberAlreadyExistsException ex)
+    public ResponseEntity<GlobalResponse<?>> handlePhoneNumberExists(PhoneNumberAlreadyExistsException ex)
     {
-        GlobalResponse response = GlobalResponse.builder()
-                .responseCode(HttpStatus.CONFLICT.toString())
-                .responseMessage(ex.getMessage())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(
+                GlobalResponse.builder()
+                        .responseCode("409")
+                        .responseMessage(ex.getMessage())
+                        .build(),
+                HttpStatus.CONFLICT
+        );
     }
 
     // Additional phone number exists
     @ExceptionHandler(AlternativePhoneNumberExistsException.class)
-    public ResponseEntity<GlobalResponse> handleAdditionalPhoneNumberExists(AlternativePhoneNumberExistsException ex)
+    public ResponseEntity<GlobalResponse<?>> handleAdditionalPhoneNumberExists(AlternativePhoneNumberExistsException ex)
     {
-        GlobalResponse response = GlobalResponse.builder()
-                .responseCode(HttpStatus.CONFLICT.toString())
-                .responseMessage(ex.getMessage())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(
+                GlobalResponse.builder()
+                        .responseCode("409")
+                        .responseMessage(ex.getMessage())
+                        .build(),
+                HttpStatus.CONFLICT
+        );
     }
 
     // Government ID already exists
     @ExceptionHandler(GovernmentIdExistsException.class)
-    public ResponseEntity<GlobalResponse> handleGovernmentIdExists(GovernmentIdExistsException ex)
+    public ResponseEntity<GlobalResponse<?>> handleGovernmentIdExists(GovernmentIdExistsException ex)
     {
-        GlobalResponse response = GlobalResponse.builder()
-                .responseCode(HttpStatus.CONFLICT.toString())
-                .responseMessage(ex.getMessage())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(
+                GlobalResponse.builder()
+                        .responseCode("409")
+                        .responseMessage(ex.getMessage())
+                        .build(),
+                HttpStatus.CONFLICT
+        );
     }
 
     // Input validations
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<GlobalResponse> handleValidationErrors(MethodArgumentNotValidException ex)
+    public ResponseEntity<GlobalResponse<?>> handleValidationErrors(MethodArgumentNotValidException ex)
     {
         String errorMessage = ex.getBindingResult().getFieldErrors()
                 .stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
 
-        GlobalResponse response = GlobalResponse.builder()
+        GlobalResponse<?> response = GlobalResponse.builder()
                 .responseCode(HttpStatus.BAD_REQUEST.toString())
                 .responseMessage(errorMessage)
                 .build();
@@ -80,67 +87,77 @@ public class GlobalExceptionHandler
 
     // Optimistic Locking
     @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
-    public ResponseEntity<GlobalResponse> handleOptimisticLockingFailure(ObjectOptimisticLockingFailureException ex)
+    public ResponseEntity<GlobalResponse<?>> handleOptimisticLockingFailure(ObjectOptimisticLockingFailureException ex)
     {
-        GlobalResponse response = GlobalResponse.builder()
+        GlobalResponse<?> response = GlobalResponse.builder()
                 .responseCode(HttpStatus.CONFLICT.toString())
                 .responseMessage("This record was updated by another process. Please refresh and try again.")
                 .build();
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
+    // account not found
     @ExceptionHandler(AccountNotFoundException.class)
-    public GlobalResponse<?> handleAccountNotFound(AccountNotFoundException ex)
+    public ResponseEntity<GlobalResponse<?>> handleAccountNotFound(AccountNotFoundException ex)
     {
-        return GlobalResponse.builder()
+        GlobalResponse<?> response = GlobalResponse.builder()
                 .responseCode(AccountUtils.CUSTOMER_NOT_FOUND_CODE)
                 .responseMessage(AccountUtils.CUSTOMER_NOT_FOUND)
                 .build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    // sending from-to same account
     @ExceptionHandler(SameAccountTransferException.class)
-    public GlobalResponse<?> handleSameAccountTransfer(SameAccountTransferException ex)
+    public ResponseEntity<GlobalResponse<?>> handleSameAccountTransfer(SameAccountTransferException ex)
     {
-        return GlobalResponse.builder()
+        GlobalResponse<?> response = GlobalResponse.builder()
                 .responseCode(AccountUtils.SAME_ACCOUNT_TRANSFER_CODE)
                 .responseMessage(AccountUtils.SAME_ACCOUNT_TRANSFER)
                 .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
+    // amount exceeds limit
     @ExceptionHandler(ExceedsTransferLimitException.class)
-    public GlobalResponse<?> handleExceedsTransferLimit(ExceedsTransferLimitException ex)
+    public ResponseEntity<GlobalResponse<?>> handleExceedsTransferLimit(ExceedsTransferLimitException ex)
     {
-        return GlobalResponse.<CustomerResponse>builder()
+        GlobalResponse<?> response = GlobalResponse.<CustomerResponse>builder()
                 .responseCode(AccountUtils.EXCEEDS_TRANSFER_LIMIT_CODE)
                 .responseMessage(AccountUtils.EXCEEDS_TRANSFER_LIMIT)
                 .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InsufficientResourcesException.class)
-    public GlobalResponse<?> handleInsufficientResources(InsufficientResourcesException ex)
+    public ResponseEntity<GlobalResponse<?>> handleInsufficientResources(InsufficientResourcesException ex)
     {
-        return GlobalResponse.<CustomerResponse>builder()
+        GlobalResponse<?> response = GlobalResponse.<CustomerResponse>builder()
                 .responseCode(AccountUtils.INSUFFICIENT_BALANCE_CODE)
                 .responseMessage(AccountUtils.INSUFFICIENT_BALANCE)
                 .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
+    // username already taken
     @ExceptionHandler(UsernameAlreadyUsedException.class)
-    public GlobalResponse<?> handleUsernameAlreadyUsed(UsernameAlreadyUsedException ex)
+    public ResponseEntity<GlobalResponse<?>> handleUsernameAlreadyUsed(UsernameAlreadyUsedException ex)
     {
-        return GlobalResponse.<AuthResponse>builder()
+        GlobalResponse<?> response = GlobalResponse.<AuthResponse>builder()
                 .responseCode(AccountUtils.USERNAME_ALREADY_TAKEN_CODE)
                 .responseMessage(AccountUtils.USERNAME_ALREADY_TAKEN)
                 .build();
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
     // GENERIC
     @ExceptionHandler(Exception.class)
-    public GlobalResponse<?> handleGenericException(Exception ex)
+    public ResponseEntity<GlobalResponse<?>> handleGenericException(Exception ex)
     {
-        return GlobalResponse.builder()
+        GlobalResponse<?> response = GlobalResponse.builder()
                 .responseCode("500")
                 .responseMessage("Internal server error")
                 .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
